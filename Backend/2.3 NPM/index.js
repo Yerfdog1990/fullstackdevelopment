9c3aa1a6aa5385f9419497fda36361fcbb4e98af
Yerfdog1990 <142722809+Yerfdog1990@users.noreply.github.com.>
2025-03-22 18:45:44 +0700

    Using NMP package manager.

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+
+import generateName from 'sillyname';
+import {randomSuperhero} from 'superheroes';
+
+
+const sillyName = generateName();
+const name = randomSuperhero();
+console.log(`My name is ${sillyName} and my superhero name is ${name}.`);
